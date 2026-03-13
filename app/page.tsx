543bcfcf3c0d09179f8541a991d8d4928bd896e6
devendhra <devad4828@gmail.com>
2026-03-13 07:26:06 +0530

    Removed niches

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ export default function Home() {
     <div>
       <h1>Vaazhthukkal Vaazhthukkal From Fusion 24</h1>
       <h2>Need to implement Fusion 24 Landing Page</h2>
-      <h2> Deva valga</h2>
-      <h2>Sankar loosu paiyale</h2>
     </div>
   );
 }
